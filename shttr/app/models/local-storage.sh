cb10111e2616cc8a9b7636a92bb58a6c10e7943e
Caleb Stein <caleb@calebstein.net>
2023-09-18 00:19:49 -0700

    add substitutions to decode some special characters

--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -3,4 +3,4 @@ for file in $(/bin/ls -t ${SHTTR_APP}/assets/storage/${CONTROLLER}); do
   message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <a href=/cgi-bin/local-storage.sh?a=delete\&id=$(sha256sum $comment | head -c 16) data-turbo-method='get' data-turbo-confirm='Are you sure?'>Delete</a></p>"
 done
 
-echo $message
+echo $message | sed "s/%3F/?/g;s/%27/'/g;s/%2C/,/g;s/%22/\"/g;s/%3A/:/g;s/%3B/;/g"
