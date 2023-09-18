c5f561a173569258640fb4f9fe7b91806d2e06ec
Caleb Stein <caleb@calebstein.net>
2023-09-17 21:47:43 -0700

    added delete links to local storage demo

--- shttr/app/controllers/local-storage.sh
+++ shttr/app/controllers/local-storage.sh
@@ -1,7 +1,13 @@
 . ${SHTTR_LIB}/io
 
+parse_input
+
 TITLE="Local Storage Demo"
 export TITLE
+
+if [ "$a" = "delete" ]; then
+  delete_file $id
+fi
 
 FORM_DATA=$(sh ${SHTTR_APP}/models/local-storage-form.sh)
 MODEL_DATA=$(sh ${SHTTR_APP}/models/${CONTROLLER})
--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -1,6 +1,6 @@
 for file in $(/bin/ls -t ${SHTTR_APP}/assets/storage/${CONTROLLER}); do
   comment=${SHTTR_APP}/assets/storage/${CONTROLLER}/${file}
-  message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment})</p>"
+  message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <a href=/cgi-bin/local-storage.sh?a=delete\&id=$(sha256sum $comment | head -c 16) data-turbo-method='get' data-turbo-confirm='Are you sure?'>Delete</a></p>"
 done
 
 echo $message
