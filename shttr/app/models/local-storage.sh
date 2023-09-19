4a77a0239886031bb07539fa24eaa0de3accb461
Caleb Stein <caleb@calebstein.net>
2023-09-19 01:14:35 -0700

    fixed stray p tag

--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -1,7 +1,7 @@
 for file in $(/bin/ls -t ${SHTTR_APP}/assets/storage/${CONTROLLER}); do
   comment=${SHTTR_APP}/assets/storage/${CONTROLLER}/${file}
   filename=$(sha256sum $comment | head -c 16)
-  message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <button class='secondary' data-turbo-confirm='Are you sure?' form='${filename}' style='padding:4px;'>Delete</button><form id='${filename}' action='/cgi-bin/local-storage.sh' method='post'><input type='hidden' name='_method' value='delete'><input type='hidden' name='id' value='${filename}'></form></p>"
+  message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <button class='secondary' data-turbo-confirm='Are you sure?' form='${filename}' style='padding:4px;'>Delete</button></p><form id='${filename}' action='/cgi-bin/local-storage.sh' method='post'><input type='hidden' name='_method' value='delete'><input type='hidden' name='id' value='${filename}'></form>"
 done
 
 echo $message | sed "s/%3F/?/g;s/%27/'/g;s/%2C/,/g;s/%22/\"/g;s/%3A/:/g;s/%3B/;/g;s/%21/!/g;"
