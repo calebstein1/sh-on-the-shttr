107d43196b905c9598d37ff23a5b1a27c516d065
Caleb Stein <caleb@calebstein.net>
2023-09-26 21:32:33 -0700

    added equals to message character substitutions

--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -4,4 +4,4 @@ for file in $(/bin/ls -t ${SHTTR_APP}/assets/storage/${CONTROLLER}); do
   message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <button class='secondary' data-turbo-confirm='Are you sure?' form='${filename}' style='padding:4px;'>Delete</button></p><form id='${filename}' action='/cgi-bin/local-storage.sh' method='post'><input type='hidden' name='_method' value='delete'><input type='hidden' name='id' value='${filename}'></form>"
 done
 
-echo $message | sed "s/%3F/?/g;s/%27/'/g;s/%2C/,/g;s/%22/\"/g;s/%3A/:/g;s/%3B/;/g;s/%21/!/g;"
+echo $message | sed "s/%3D/=/;s/%3F/?/g;s/%27/'/g;s/%2C/,/g;s/%22/\"/g;s/%3A/:/g;s/%3B/;/g;s/%21/!/g;"
