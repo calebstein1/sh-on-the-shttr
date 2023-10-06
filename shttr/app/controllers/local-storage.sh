ee0291f19de1133a95fd13fff75eb933a4c28e3f
Caleb Stein <caleb@calebstein.net>
2023-10-06 13:06:50 -0700

    Added first version of shttrdb, with reading, writing, and deleting data

--- shttr/app/controllers/local-storage.sh
+++ shttr/app/controllers/local-storage.sh
@@ -1,4 +1,5 @@
 . ${SHTTR_LIB}/io
+. ${SHTTR_LIB}/shttrdb
 
 parse_input
 
@@ -6,13 +7,17 @@ TITLE="Local Storage Demo"
 export TITLE
 
 if [ "$_method" = "delete" ]; then
-  delete_file $id
+  delete_id $id from comments
 fi
 
 . ${SHTTR_APP}/models/${CONTROLLER}
 
 if [ "$REQUEST_METHOD" = "POST" ]; then
-  [ -z $_method ] && write_file "$FORM_DATA"
+  if [ -z $_method ]; then
+    file_id=$(create_table_entry in comments)
+    write_data "${file_id}" name "${name}" to comments
+    write_data "${file_id}" content "${content}" to comments
+  fi
 fi
 
 sh ${SHTTR_APP}/views/${CONTROLLER}
--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -1,14 +1,18 @@
-for file in $(/bin/ls -t ${SHTTR_APP}/assets/storage/${CONTROLLER}); do
-  comment=${SHTTR_APP}/assets/storage/${CONTROLLER}/${file}
-  filename=$(sha256sum $comment | head -c 16)
-  message="${message}<p><strong>$(head -n1 ${comment}):</strong> $(tail -n1 ${comment}) <button class='secondary' data-turbo-confirm='Are you sure?' form='${filename}' style='padding:4px;'>Delete</button></p><form id='${filename}' action='/cgi-bin/local-storage.sh' method='post'><input type='hidden' name='_method' value='delete'><input type='hidden' name='id' value='${filename}'></form>"
+read_each from comments
+for id in $comments_id_list; do
+  MODEL_DATA=$(cat <<EOF
+<p>
+  <strong>Name:</strong> $(comments_${id} name)<br>
+  <strong>Comment:</strong> $(comments_${id} content)
+  <button class="secondary" style="padding:4px;" form="${id}" data-turbo-confirm="Are you sure?">Delete</button>
+</p>
+<form id="${id}" method="post">
+  <input type="hidden" name="_method" value="delete" />
+  <input type="hidden" name="id" value="${id}" />
+</form>
+${MODEL_DATA}
+EOF
+)
 done
-
-MODEL_DATA=$(url_string_decode "$message")
+MODEL_DATA=$(echo $MODEL_DATA | tr "\n" " ")
 export MODEL_DATA
-
-name=$(url_string_decode "$name")
-content=$(url_string_decode "$content")
-
-FORM_DATA=$(echo "${name}\n${content}")
-export FORM_DATA
