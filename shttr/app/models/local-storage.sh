e3215343f1abcf1670bdd7d587bf0fdbcd446e78
Caleb Stein <caleb@calebstein.net>
2023-10-07 09:23:43 -0700

    moved html loop for comments into view script... not the most elegant, but the best solution for now

--- shttr/app/models/local-storage.sh
+++ shttr/app/models/local-storage.sh
@@ -1,18 +1,1 @@
 read_each from comments
-for id in $comments_id_list; do
-  MODEL_DATA=$(cat <<EOF
-<p>
-  <strong>Name:</strong> $(comments_${id} name)<br>
-  <strong>Comment:</strong> $(comments_${id} content)
-  <button class="secondary" style="padding:4px;" form="${id}" data-turbo-confirm="Are you sure?">Delete</button>
-</p>
-<form id="${id}" method="post">
-  <input type="hidden" name="_method" value="delete" />
-  <input type="hidden" name="id" value="${id}" />
-</form>
-${MODEL_DATA}
-EOF
-)
-done
-MODEL_DATA=$(echo $MODEL_DATA | tr "\n" " ")
-export MODEL_DATA
